75b9667198076a822ce2f1fa32fadd7e0d692443
Park June Chul <pjc0247@naver.com>
2015-06-25 11:32:41 +0900

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,34 @@
+#include "lockable.h"
+
+#include <thread>
+#include <vector>
+
+class test{
+public:
+	test(int initial_value) :
+		value(initial_value){
+	}
+
+	int value;
+};
+
+void main(){
+  auto a = make_lockable<test>(0);
+	std::vector<std::thread> threads;
+
+	for(int i=0;i<10;i++){
+		threads.push_back(
+			std::thread([&a](){
+				for(int i=0;i<10000;i++){
+					a.lock([&a](){
+						a->value ++;
+					});
+				}
+			}));
+	}
+	for(auto &t : threads){
+		t.join();
+	}
+	
+	printf("%d\n", a->value);
+}
