8096ccf3178b92b4b28c46449366466443782237
Park June Chul <pjc0247@naver.com>
2015-06-25 11:33:35 +0900

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -5,30 +5,30 @@
 
 class test{
 public:
-	test(int initial_value) :
-		value(initial_value){
-	}
+  test(int initial_value) :
+    value(initial_value){
+  }
 
-	int value;
+  int value;
 };
 
 void main(){
   auto a = make_lockable<test>(0);
-	std::vector<std::thread> threads;
+  std::vector<std::thread> threads;
 
-	for(int i=0;i<10;i++){
-		threads.push_back(
-			std::thread([&a](){
-				for(int i=0;i<10000;i++){
-					a.lock([&a](){
-						a->value ++;
-					}); 
-				}
-			}));
-	}
-	for(auto &t : threads){
-		t.join();
-	}
-	
-	printf("%d\n", a->value);
+  for(int i=0;i<10;i++){
+    threads.push_back(
+      std::thread([&a](){
+        for(int i=0;i<10000;i++){
+          a.lock([&a](){
+            a->value ++;
+          }); 
+        }
+      }));
+  }
+  for(auto &t : threads){
+    t.join();
+  }
+  
+  printf("%d\n", a->value);
 }
